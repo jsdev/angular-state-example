67395a10e47dc9be3df5baca33c79662650dfcd6
Anthony "Ryan" Delorie <jsdev@users.noreply.github.com>
2024-09-05 12:04:53 -0400

    Update app.module.ts
    Updated to use Angular Material

--- app.module.ts
+++ app.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+// Angular Material imports
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
 import { AppComponent } from './app.component';
 import { PlannerComponent } from './components/planner.component';
 import { ParkSelectionComponent } from './components/park-selection.component';
@@ -25,12 +39,23 @@ import { PlannerEffects } from './store/planner.effects';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     StoreModule.forRoot({ planner: plannerReducer }),
     EffectsModule.forRoot([PlannerEffects]),
-    StoreDevtoolsModule.instrument()
+    StoreDevtoolsModule.instrument(),
+    MatToolbarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatButtonModule,
+    MatListModule,
+    MatProgressSpinnerModule
   ],
   providers: [WeatherService, PlannerService],
   bootstrap: [AppComponent]
